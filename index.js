6f4df7c502bb2c92356e1cc55a204d37fe5e69e6
Mamun172136 <ma1536204@gmail.com>
2023-08-21 21:30:03 +0600

    [done] delete cart item

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
-
+const { ObjectId } = require("mongodb");
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -56,6 +56,14 @@ async function run() {
       const item = req.body;
       console.log(item);
       const result = await cartCollection.insertOne(item);
+      res.send(result);
+    });
+
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
     });
 
